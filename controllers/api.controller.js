1621115e3a7089d82e5a5459b7a2864fc40386f2
rociosmm <rociosmm@yahoo.com>
2024-05-29 00:50:47 +0100

    GET /api endpoint complete with a catch added in the controller

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,7 +1,11 @@
 const fs = require("fs/promises");
 
 exports.endpointsInfo = (req, res, next) => {
-  fs.readFile("./endpoints.json", "utf-8").then((data) => {
-    res.status(200).send(JSON.parse(data));
-  });
+  fs.readFile("./endpoints.json", "utf-8")
+    .then((data) => {
+      res.status(200).send(JSON.parse(data));
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
